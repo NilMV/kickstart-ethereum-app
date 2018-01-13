62baa6cc75348e0c6f3016d887700872444d8d74
NilMV <nil.medvedev17@gmail.com>
2018-01-13 16:35:11 +0300

    add new tests

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -39,4 +39,43 @@ describe('Campaigns', () => {
       assert.ok(factory.options.address);
       assert.ok(campaign.options.address);
   });
+
+  it('marks caller as the campaign manager', async () => {
+      const manager = await campaign.methods.manager().call();
+      assert.equal(manager, accounts[0]);
+  });
+
+  it('allows people to contribute moeny and marks them as approvers', async () => {
+      await campaign.methods.contribute().send({
+        value: '200',
+        from: accounts[1]
+      });
+      const isContributor = await campaign.methods.approvers(accounts[1]).call();
+      assert(isContributor);
+  });
+
+  it('requires a mnimum contribution', async () => {
+    try {
+        await campaign.methods.contribute().send({
+            value: '5',
+            from: accounts[1]
+        });
+        assert(false);
+    } catch (err) {
+        assert(err);
+    }   
+  });
+
+  it('allows a manager to make a payment request', async () => {
+   /* await campaign.methods
+        .createRequest('Buy something', '100', accounts[1])
+        .send({
+            from: accounts[0],
+            gas: '1000000'
+        });*/
+    //const request = await campaign.methods.request(0).call();
+
+    //assert.equal('Buy something', request.decsription);
+      
+  });
 })
